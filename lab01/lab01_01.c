58755dec0c51dae22ae4e48375e85cca9fbbd482
Lakshya Salgotra <lakshyasalgotra57@gmail.com>
2025-10-28 12:24:52 +0530

    Create lab01_01.c

---
+++ lab01/lab01_01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Name- Lakshya Salgotra\nSAP ID - 590026601\nCourse- BCA\nBatch- 6\n");
+    printf("\n ---------- \n");
+    printf("Hello World\n");
+    return 0;
+}
